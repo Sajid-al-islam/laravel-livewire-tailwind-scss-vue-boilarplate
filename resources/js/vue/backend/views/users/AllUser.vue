7676dcc1fd33fa7c7b751b1ee5c0b20531837ee4
Md-shefat-masum <mshefat924@gmail.com>
2022-12-24 23:56:20 +0600

    table action

--- resources/js/vue/backend/views/users/AllUser.vue
+++ resources/js/vue/backend/views/users/AllUser.vue
@@ -27,13 +27,20 @@
                             </td>
                             <td><span class="badge bg-label-success me-1">Completed</span></td>
                             <td>
-                                <div class="dropdown">
-                                    <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown" aria-expanded="false"><i class="ti ti-dots-vertical"></i></button>
-                                    <div class="dropdown-menu" style="">
-                                        <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-pencil me-1"></i> Edit</a>
-                                        <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-trash me-1"></i> Delete</a>
-                                    </div>
+                                <div class="table_actions">
+                                    <a href="#" class="btn btn-sm btn-outline-secondary">
+                                        <i class="fa fa-gears"></i>
+                                    </a>
+                                    <ul>
+                                        <li v-for="i in 5" :key="i">
+                                            <a href="#/module/details/1" class="">
+                                                <i class="fa text-primary fa-list"></i>
+                                                module details
+                                            </a>
+                                        </li>
+                                    </ul>
                                 </div>
+
                             </td>
                         </tr>
                     </tbody>
