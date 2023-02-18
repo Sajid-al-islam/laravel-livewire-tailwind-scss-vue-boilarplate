1dc4a52bf99896094629c6af6af7f42b9473fc53
Md-shefat-masum <mshefat924@gmail.com>
2023-02-18 14:51:59 +0600

    module updated

--- resources/js/vue/backend/views/users/CreateUserCanvas.vue
+++ resources/js/vue/backend/views/users/CreateUserCanvas.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="canvas_backdrop"
-        :class="{active:get_show_create_canvas}"
-        @click="$event.target.classList.contains('canvas_backdrop') && set_show_create_canvas(false)">
-        <div class="content right" v-if="get_show_create_canvas">
+        :class="{active:this[`get_${store_prefix}_show_create_canvas`]}"
+        @click="$event.target.classList.contains('canvas_backdrop') && call_store(`set_${store_prefix}_show_create_canvas`,false)">
+        <div class="content right" v-if="this[`get_${store_prefix}_show_create_canvas`]">
             <div class="content_header">
-                <h3 class="offcanvas-title">Create User</h3>
-                <i @click="set_show_create_canvas(false)" class="fa fa-times"></i>
+                <h3 class="offcanvas-title">Create {{route_prefix}}</h3>
+                <i @click="call_store(`set_${store_prefix}_show_create_canvas`, false)" class="fa fa-times"></i>
             </div>
             <div class="cotent_body user_canvas_create_form">
                 <div class=" form-group d-grid align-content-start gap-1 mb-2 " >
@@ -50,7 +50,7 @@
                     />
                 </div>
                 <div class=" form-group text-center mb-2 " >
-                    <button @click.prevent="upload_user_create_canvas_input" type="button" class="btn btn-outline-info">Submit</button>
+                    <button @click.prevent="call_store(`upload_${store_prefix}_create_canvas_input`)" type="button" class="btn btn-outline-info">Submit</button>
                 </div>
             </div>
         </div>
@@ -60,16 +60,30 @@
 <script>
 import { mapActions, mapGetters, mapMutations } from 'vuex'
 import InputField from '../components/InputField.vue'
+
+/** store and route prefix for export object use */
+import PageSetup from "./PageSetup";
+const {route_prefix, store_prefix, layout_title} = PageSetup;
 export default {
     components: { InputField },
+    data: function(){
+        return {
+            route_prefix,
+            store_prefix,
+            layout_title,
+        }
+    },
     methods: {
         ...mapActions(['upload_user_create_canvas_input']),
         ...mapMutations([
-            'set_show_create_canvas',
+            'set_user_show_create_canvas',
         ]),
+        call_store: function(name, params=null){
+            this[name](params)
+        },
     },
     computed: {
-        ...mapGetters(['get_show_create_canvas'])
+        ...mapGetters(['get_user_show_create_canvas'])
     }
 }
 </script>
--- resources/js/vue/backend/views/users/components/UserManagementModal.vue
+++ resources/js/vue/backend/views/users/components/UserManagementModal.vue
@@ -1,30 +1,30 @@
 <template>
     <div class="multiple_select_body">
-        <div class="multiple_select_data" @click="set_show_user_management_modal(true)">
-            <div v-for="user in get_selected_users" :key="user.id" class="item">
+        <div class="multiple_select_data" @click="call_store(`set_user_show_management_modal`,true)">
+            <div v-for="user in this[`get_user_selected`]" :key="user.id" class="item">
                 {{ user.first_name }}
                 {{ user.last_name }}
             </div>
-            <div class="btn btn-sm btn-outline-danger" v-if="!get_selected_users.length">
+            <div class="btn btn-sm btn-outline-danger" v-if="!this[`get_user_selected`].length">
                 no user selected
             </div>
         </div>
 
-        <div class="multiple_select_modal" v-if="get_show_user_management_modal">
-            <div class="multiple_select_modal_backdrop" @click="set_show_user_management_modal(false)"></div>
+        <div class="multiple_select_modal" v-if="this[`get_user_show_management_modal`]">
+            <div class="multiple_select_modal_backdrop" @click="call_store(`set_user_show_management_modal`,false)"></div>
             <div class="multiple_select_modal_body custom_scroll">
                 <div class="header">
                     <div class="search">
-                        <input @keyup="set_users_search_key($event.target.value)" type="text" class="rounded-pill form-control">
+                        <input @keyup="call_store(`set_user_search_key`,$event.target.value)" type="text" class="rounded-pill form-control">
                     </div>
                     <div class="action_btns">
-                        <a @click.prevent="set_clear_selected_users()" v-if="get_selected_users.length" href="#" class="btn rounded-pill btn-outline-danger me-2"><i class="fa fa-trash"></i> remove selected</a>
-                        <a @click.prevent="set_show_create_canvas()" href="#" class="btn rounded-pill btn-outline-primary"><i class="fa fa-pencil"></i> create</a>
+                        <a @click.prevent="call_store(`set_clear_selected_users`)" v-if="this[`get_user_selected`].length" href="#" class="btn rounded-pill btn-outline-danger me-2"><i class="fa fa-trash"></i> remove selected</a>
+                        <a @click.prevent="call_store(`set_user_show_create_canvas`,true)" href="#" class="btn rounded-pill btn-outline-primary"><i class="fa fa-pencil"></i> create</a>
                     </div>
                 </div>
                 <div class="selected">
-                    <div class="item" v-for="user in get_selected_users" :key="user.id">
-                        <button @click.prevent="set_selected_users(user)" class="btn rounded-pill btn-outline-secondary" type="button">
+                    <div class="item" v-for="user in this[`get_user_selected`]" :key="user.id">
+                        <button @click.prevent="call_store(`set_selected_users`, user)" class="btn rounded-pill btn-outline-secondary" type="button">
                             <span>
                                 {{ user.user_name }}
                             </span>
@@ -37,28 +37,28 @@
                     <table class="table table-hover table-bordered">
                         <thead class="table-light">
                             <tr>
-                                <th><input @click="set_select_all_users()" type="checkbox" class="form-check-input check_all"></th>
-                                <user-table-th :sort="true" :ariaLable="'id'" :tkey="'id'" :title="'ID'" />
-                                <user-table-th :sort="true" :tkey="'photo'" :title="'Photo'" />
-                                <user-table-th :sort="true" :tkey="'first_name'" :title="'Name'" />
-                                <!-- <user-table-th :sort="true" :tkey="'email'" :title="'Email'" /> -->
-                                <user-table-th :sort="true" :tkey="'mobile_number'" :title="'Mobile NO'" />
-                                <!-- <user-table-th :sort="true" :tkey="'status'" :title="'Status'" /> -->
+                                <th><input @click="call_store(`set_select_all_users`)" type="checkbox" class="form-check-input check_all"></th>
+                                <user-table-th :sort="true" :ariaLable="`id`" :tkey="`id`" :title="`ID`" />
+                                <user-table-th :sort="true" :tkey="`photo`" :title="`Photo`" />
+                                <user-table-th :sort="true" :tkey="`first_name`" :title="`Name`" />
+                                <!-- <user-table-th :sort="true" :tkey="`email`" :title="`Email`" /> -->
+                                <user-table-th :sort="true" :tkey="`mobile_number`" :title="`Mobile NO`" />
+                                <!-- <user-table-th :sort="true" :tkey="`status`" :title="`Status`" /> -->
                                 <th aria-label="actions">Actions</th>
                             </tr>
                         </thead>
                         <tbody class="table-border-bottom-0">
-                            <tr v-for="item in get_users.data" :key="item.id">
+                            <tr v-for="item in this[`get_users`].data" :key="item.id">
                                 <td>
-                                    <input v-if="check_if_user_is_selected(item)" :data-id="item.id" checked @change="set_selected_users(item)" type="checkbox" class="form-check-input">
-                                    <input v-else @change="set_selected_users(item)" type="checkbox" class="form-check-input">
+                                    <input v-if="check_if_user_is_selected(item)" :data-id="item.id" checked @change="call_store(`set_selected_users`, item)" type="checkbox" class="form-check-input">
+                                    <input v-else @change="call_store(`set_selected_users`, item)" type="checkbox" class="form-check-input">
                                 </td>
                                 <td>{{ item.id }}</td>
                                 <td>
                                     <img :src="`${item.photo_url}`" style="height:30px;" alt="Avatar" class="rounded-circle" />
                                 </td>
                                 <td >
-                                    <span class="text-warning cursor_pointer" @click.prevent="set_user(item)">
+                                    <span class="text-warning cursor_pointer" @click.prevent="call_store(`set_user`, item)">
                                         {{ item.first_name }} {{ item.last_name }}
                                     </span>
                                 </td>
@@ -72,7 +72,7 @@
                                 </td> -->
                                 <td>
                                     <div class="table_actions">
-                                        <a href="#" @click.prevent="()=>''" class="btn btn-sm btn-outline-secondary">
+                                        <a href="#" @click.prevent="()=>``" class="btn btn-sm btn-outline-secondary">
                                             <i class="fa fa-gears"></i>
                                         </a>
                                         <ul>
@@ -90,12 +90,12 @@
                     </table>
                 </div>
                 <div class="footer_modal d-flex justify-content-between align-items-start">
-                    <pagination :data="get_users" :limit="5" :size="'small'" :show-disabled="true" :align="'left'"
-                        @pagination-change-page="set_users_page">
+                    <pagination :data="get_users" :limit="5" :size="`small`" :show-disabled="true" :align="`left`"
+                        @pagination-change-page="handle_pagination">
                         <span slot="prev-nav"><i class="fa fa-angle-left"></i> Previous</span>
                         <span slot="next-nav">Next <i class="fa fa-angle-right"></i></span>
                     </pagination>
-                    <button @click.prevent="set_show_management_modal(false)" href="#" class="btn rounded-pill btn-outline-secondary">
+                    <button @click.prevent="call_store(`set_user_show_management_modal`,false)" href="#" class="btn rounded-pill btn-outline-secondary">
                         <i class="fa fa-floppy-disk"></i>
                         Save & Close
                     </button>
@@ -108,46 +108,63 @@
 </template>
 
 <script>
-import { mapActions, mapGetters, mapMutations } from 'vuex'
-import CreateUserCanvas from '../CreateUserCanvas.vue';
-import UserTableTh from './UserTableTh.vue';
+import { mapActions, mapGetters, mapMutations } from "vuex"
+import CreateUserCanvas from "../CreateUserCanvas.vue";
+import UserTableTh from "./UserTableTh.vue";
+/** store and route prefix for export object use */
+import PageSetup from "../PageSetup";
+const {route_prefix, store_prefix, pagination_limits} = PageSetup;
+
 export default {
     components: { UserTableTh, CreateUserCanvas },
+    data: function(){
+        return {
+            route_prefix,
+            store_prefix,
+            pagination_limits
+        }
+    },
     created: function(){
-        this.set_users_paginate(9);
+        this.set_user_paginate(9);
         this.fetch_users();
     },
     methods: {
-        ...mapActions(['fetch_users']),
+        ...mapActions([`fetch_users`]),
         ...mapMutations([
-            'set_users_paginate',
-            'set_users_page',
-            'set_users_search_key',
-            'set_users_orderByCol',
-            'set_user',
-            'set_selected_users',
-            'set_select_all_users',
-            'set_clear_selected_users',
-            'check_if_user_is_selected',
-            'set_show_selected',
-            'set_show_create_canvas',
-            'set_show_user_management_modal',
+            `set_user_paginate`,
+            `set_user_page`,
+            `set_user_search_key`,
+            `set_user_orderByCol`,
+            `set_user`,
+            `set_selected_users`,
+            `set_select_all_users`,
+            `set_clear_selected_users`,
+            `set_user_show_selected`,
+            `set_user_show_create_canvas`,
+            `set_user_show_management_modal`,
         ]),
 
         check_if_user_is_selected: function(user){
-            let check_index = this.get_selected_users.findIndex((i) => i.id == user.id);
+            let check_index = this.get_user_selected.findIndex((i) => i.id == user.id);
             if(check_index >= 0){
                 return true;
             }else{
                 return false;
             }
         },
+
+        call_store: function(name, params=null){
+            this[name](params)
+        },
+        handle_pagination: function(page=1){
+            return this[`set_${store_prefix}_page`](page);
+        },
     },
     computed: {
         ...mapGetters([
-            'get_users',
-            'get_selected_users',
-            'get_show_user_management_modal'
+            `get_users`,
+            `get_user_selected`,
+            `get_user_show_management_modal`,
         ]),
     }
 };
